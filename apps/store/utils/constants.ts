6683d8f2d5bbb36421013110469f0e4ae1d6d0e2
Luke Bennett <luke.bennett@brighte.com.au>
2021-09-30 22:40:37 +1000

    Add seo settings

--- apps/store/utils/constants.ts
+++ apps/store/utils/constants.ts
@@ -1,3 +1,24 @@
-export const VENDOR_NAME = 'Port Macquarie Tender Centre';
-export const VENDOR_LOGO_URL =
-  'https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg';
+const title = '@thatsferntastic';
+const description =
+  '@thatsferntastic is an online store focusing on the little things that make you smile. Pencil cases, planner pouches and coin purses, all handmade in Australia, plus accessories, stationery and stickers to add a little more sunshine to your day. Offering flat rate shipping from my little studio on the coast of NSW.';
+
+export const siteSettings = {
+  title,
+  titleTemplate: `%s: pencil cases, planner pouches and coin purses, accessories, stationery and stickers to add a little more sunshine to your day.`,
+  description,
+  openGraph: {
+    title,
+    description,
+    type: 'website',
+    locale: 'en_AU',
+    url: 'https://www.thatsferntastic.com.au',
+    site_name: title,
+    images: [
+      {
+        url: 'https://cdn.sanity.io/images/vxhsnaeh/production/5b0f778989438587159b584d5258a65c73f0217b-1800x1200.png',
+        height: 1800,
+        width: 1200,
+      },
+    ],
+  },
+};

1027ddf9774f3cfc5b00658d2fc02d228198b2b1
Luke Bennett <luke.bennett@brighte.com.au>
2021-09-30 22:41:38 +1000

    Add utilty to format currency

--- apps/store/utils/index.ts
+++ apps/store/utils/index.ts
@@ -2,6 +2,15 @@ export function classNames(...classes: unknown[]): string {
   return classes.filter(Boolean).join(' ');
 }
 
+const formatter = new Intl.NumberFormat('en-AU', {
+  style: 'currency',
+  currency: 'AUD',
+});
+
+export const formatCurrency = (dollars: number): string => {
+  return formatter.format(dollars);
+};
+
 export function slugify(text: string): string {
   return (
     text

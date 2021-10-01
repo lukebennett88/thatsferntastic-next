201564de3f989eeca3d255d835ce3ae7d51ee335
Luke Bennett <luke.bennett@brighte.com.au>
2021-10-01 16:02:58 +1000

    Add custom Tailwind theme

--- apps/store/tailwind.config.js
+++ apps/store/tailwind.config.js
@@ -1,8 +1,70 @@
+const colors = require('tailwindcss/colors');
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
+  mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: colors.gray,
+        indigo: colors.indigo,
+        cyan: {
+          50: '#ebfcff',
+          100: '#e1fafe',
+          200: '#c3f4fd',
+          300: '#a4f1fe',
+          400: '#76e6fa',
+          500: '#3fdcf8',
+          600: '#1cd2f2',
+          700: '#1abedb',
+          800: '#1e8c9f',
+          900: '#236a76',
+        },
+        teal: {
+          50: '#edfafa',
+          100: '#d5f5f6',
+          200: '#afeeef',
+          300: '#7ee0e2',
+          400: '#16c7ca',
+          500: '#069fa2',
+          600: '#047f81',
+          700: '#037072',
+          800: '#055b5c',
+          900: '#015051',
+        },
+        pink: {
+          50: '#fdf2f9',
+          100: '#fce8f4',
+          200: '#fad2ea',
+          300: '#f8b4dd',
+          400: '#f17ec3',
+          500: '#e746a7',
+          600: '#d61f8d',
+          700: '#bf127a',
+          800: '#991564',
+          900: '#751a51',
+        },
+      },
+      fontFamily: {
+        mono: ['Courier Prime', 'Courier New', ...defaultTheme.fontFamily.mono],
+        sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
+      },
+      typography: theme => ({
+        DEFAULT: {
+          css: {
+            'h1, h2, h3, h4': {
+              color: theme('colors.pink.500'),
+              fontWeight: theme('fontWeight.medium'),
+            },
+            img: {
+              width: theme('width.full'),
+            },
+          },
+        },
+      }),
+    },
   },
   variants: {
     extend: {},

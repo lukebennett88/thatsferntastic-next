33bc8ec130e57658921f5263a2fe8d316f53c7c5
Luke Bennett <luke.bennett@brighte.com.au>
2021-10-01 11:57:28 +1000

    Add getTopSellingProducts query

---
+++ apps/store/graphql/get-top-selling-products.ts
@@ -0,0 +1,63 @@
+import { gql } from '@ts-gql/tag';
+
+import type { Client } from '../utils/apollo-client';
+
+const TOP_SELLING_PRODUCTS = gql`
+  query TopSellingProducts($first: Int = 4, $query: String) {
+    products(first: $first, sortKey: BEST_SELLING, query: $query) {
+      edges {
+        node {
+          id
+          compareAtPriceRange {
+            minVariantPrice {
+              amount
+            }
+          }
+          handle
+          images(first: 1) {
+            edges {
+              node {
+                altText
+                id
+                originalSrc
+              }
+            }
+          }
+          priceRange {
+            minVariantPrice {
+              amount
+            }
+          }
+          title
+          variants(first: 1) {
+            edges {
+              node {
+                id
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+` as import('../../../__generated__/ts-gql/TopSellingProducts').type;
+
+export type TopSellingProducts =
+  typeof TOP_SELLING_PRODUCTS['___type']['result']['products']['edges'];
+
+export async function getTopSellingProducts(
+  client: Client,
+  productType?: string,
+  first?: number
+): Promise<TopSellingProducts> {
+  const { data } = await client.query({
+    query: TOP_SELLING_PRODUCTS,
+    variables: {
+      first,
+      query: `${
+        productType ? `product_type:${productType}, ` : ''
+      }available_for_sale:true`,
+    },
+  });
+  return data?.products.edges;
+}

2320529ba2f888a706aeb03cd39d0b02f57f1816
Luke Bennett <luke.bennett@brighte.com.au>
2021-10-02 08:47:48 +1000

    Add cart popover

--- apps/store/components/desktop-menu/desktop-menu.tsx
+++ apps/store/components/desktop-menu/desktop-menu.tsx
@@ -10,6 +10,30 @@ import * as React from 'react';
 
 import { classNames } from '../../utils';
 
+const products = [
+  {
+    id: 1,
+    name: 'Throwback Hip Bag',
+    href: '#',
+    color: 'Salmon',
+    imageSrc:
+      'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg',
+    imageAlt:
+      'Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt.',
+  },
+  {
+    id: 2,
+    name: 'Medium Stuff Satchel',
+    href: '#',
+    color: 'Blue',
+    imageSrc:
+      'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg',
+    imageAlt:
+      'Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.',
+  },
+  // More products...
+];
+
 interface Navigation {
   categories: {
     id: string;
@@ -234,18 +258,68 @@ export function DesktopMenu({
               </div>
 
               {/* Cart */}
-              <div className="flow-root ml-4 lg:ml-6">
-                <a href="#" className="flex items-center p-2 -m-2 group">
+              <Popover className="flow-root ml-4 text-sm lg:relative lg:ml-8">
+                <Popover.Button className="flex items-center p-2 -m-2 group">
                   <ShoppingBagIcon
-                    aria-hidden="true"
                     className="flex-shrink-0 w-6 h-6 text-gray-400 group-hover:text-gray-500"
+                    aria-hidden="true"
                   />
                   <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
                     0
                   </span>
                   <span className="sr-only">items in cart, view bag</span>
-                </a>
-              </div>
+                </Popover.Button>
+                <Transition
+                  as={React.Fragment}
+                  enter="transition ease-out duration-200"
+                  enterFrom="opacity-0"
+                  enterTo="opacity-100"
+                  leave="transition ease-in duration-150"
+                  leaveFrom="opacity-100"
+                  leaveTo="opacity-0"
+                >
+                  <Popover.Panel className="absolute top-16 inset-x-0 mt-px pb-6 bg-white shadow-lg sm:px-2 lg:top-full lg:left-auto lg:right-0 lg:mt-3 lg:-mr-1.5 lg:w-80 lg:rounded-lg lg:ring-1 lg:ring-black lg:ring-opacity-5 z-30">
+                    {({ close }) => (
+                      <>
+                        <h2 className="sr-only">Shopping Cart</h2>
+
+                        <div className="max-w-2xl px-4 mx-auto">
+                          <ul role="list" className="divide-y divide-gray-200">
+                            {products.map(product => (
+                              <li
+                                key={product.id}
+                                className="flex items-center py-6"
+                              >
+                                <img
+                                  src={product.imageSrc}
+                                  alt={product.imageAlt}
+                                  className="flex-none w-16 h-16 border border-gray-200 rounded-md"
+                                />
+                                <div className="flex-auto ml-4">
+                                  <h3 className="font-medium text-gray-900">
+                                    <a href={product.href}>{product.name}</a>
+                                  </h3>
+                                  <p className="text-gray-500">
+                                    {product.color}
+                                  </p>
+                                </div>
+                              </li>
+                            ))}
+                          </ul>
+                          <Link href="/cart">
+                            <a
+                              onClick={() => close()}
+                              className="block w-full px-4 py-2 text-sm font-medium text-center text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-indigo-500"
+                            >
+                              Checkout
+                            </a>
+                          </Link>
+                        </div>
+                      </>
+                    )}
+                  </Popover.Panel>
+                </Transition>
+              </Popover>
             </div>
           </div>
         </div>

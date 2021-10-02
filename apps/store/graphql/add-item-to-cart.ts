dfe119c126772ab8fbcf1b8dfe7c404033d9145a
Luke Bennett <luke.bennett@brighte.com.au>
2021-10-02 14:01:32 +1000

    Create addItemToCart function

---
+++ apps/store/graphql/add-item-to-cart.ts
@@ -0,0 +1,51 @@
+import { gql } from '@ts-gql/tag';
+
+import type { Client } from '../utils/apollo-client';
+
+const ADD_ITEM_TO_CART = gql`
+  mutation AddItemToCart(
+    $checkoutLineItemsAddLineItems: [CheckoutLineItemInput!]!
+    $checkoutLineItemsAddCheckoutId: ID!
+  ) {
+    checkoutLineItemsAdd(
+      lineItems: $checkoutLineItemsAddLineItems
+      checkoutId: $checkoutLineItemsAddCheckoutId
+    ) {
+      checkout {
+        id
+        webUrl
+      }
+    }
+  }
+` as import('../../../__generated__/ts-gql/AddItemToCart').type;
+
+export type Checkout =
+  typeof ADD_ITEM_TO_CART['___type']['result']['checkoutLineItemsAdd'];
+
+interface CheckoutLineItemsAddLineItems {
+  quantity: number;
+  variantId: string;
+}
+
+interface AddItemToCart {
+  client: Client;
+  checkoutLineItemsAddLineItems: CheckoutLineItemsAddLineItems;
+  checkoutLineItemsAddCheckoutId: string;
+}
+
+export async function addItemToCart({
+  client,
+  checkoutLineItemsAddLineItems,
+  checkoutLineItemsAddCheckoutId,
+}: AddItemToCart): Promise<Checkout | void> {
+  const { data } = await client.mutate({
+    mutation: ADD_ITEM_TO_CART,
+    variables: {
+      checkoutLineItemsAddLineItems,
+      checkoutLineItemsAddCheckoutId,
+    },
+  });
+  return data?.checkoutLineItemsAdd;
+}
+
+// checkoutLineItemsAddLineItems,

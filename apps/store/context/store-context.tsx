e14e8bf564ba3751b874eb337bd7e32dc0d54333
Luke Bennett <luke.bennett@brighte.com.au>
2021-10-02 22:20:07 +1000

    Use Shopify Buy SDK

--- apps/store/context/store-context.tsx
+++ apps/store/context/store-context.tsx
@@ -1,50 +1,170 @@
-import isEqual from 'lodash/isEqual';
+import fetch from 'isomorphic-unfetch';
 import * as React from 'react';
+import type { Cart, Client } from 'shopify-buy';
+import ShopifyBuy from 'shopify-buy';
 
-import { createCheckout, InitialCheckout } from '../graphql/create-checkout';
-import { initialiseTsGql } from '../utils/apollo-client';
+const client: Client = ShopifyBuy.buildClient(
+  {
+    domain: process.env.NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN,
+    storefrontAccessToken:
+      process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN,
+  },
+  // @ts-expect-error Types are wrong here, second param is fetch function.
+  fetch
+);
+
 import { useLocalStorageState } from '../utils/hooks';
 
-type Cart = InitialCheckout;
-
 interface CartState {
+  addVariantToCart: (variantId: string, quantity: number) => Promise<void>;
   cart?: Cart;
+  client?: Client;
+  removeLineItem: (checkoutId: string, lineItemId: string) => Promise<void>;
   setCart: React.Dispatch<React.SetStateAction<Cart | undefined>>;
+  updateLineItem: (
+    checkoutId: string,
+    lineItemId: string,
+    quantity: number
+  ) => Promise<void>;
 }
 
 const StoreContext = React.createContext<CartState | undefined>(undefined);
 
-export const CART = 'shopify_local_store__cart';
+const localStorageKey = 'shopify_checkout_id';
 
 interface StoreProviderProps {
   children: React.ReactNode;
 }
 
 export function StoreProvider({ children }: StoreProviderProps): JSX.Element {
-  const [cart, setCart] = useLocalStorageState(CART);
-  // If we don't have a cart in local storage, create a new one
-  const prevCartRef = React.useRef(cart);
+  const [cart, setCart] = useLocalStorageState(localStorageKey);
+  // // If we don't have a cart in local storage, create a new one
+  // const prevCartRef = React.useRef(cart);
+  // React.useEffect(() => {
+  //   const getNewCart = async (): Promise<void> => {
+  //     const prevCart = prevCartRef.current;
+  //     // Not sure if this comparison will work or not
+  //     if (isEqual(prevCart, cart)) {
+  //       const client = initialiseTsGql();
+  //       const newCart = await createCheckout(client);
+  //       setCart(newCart);
+  //       prevCartRef.current = cart;
+  //     }
+  //   };
+  //   // If cart as been purchased, create a new cart
+  //   if (!cart || cart?.completedAt) {
+  //     getNewCart();
+  //   }
+  // }, [cart, setCart]);
+  const isBrowser = typeof window !== 'undefined';
+
   React.useEffect(() => {
-    const getNewCart = async (): Promise<void> => {
-      const prevCart = prevCartRef.current;
-      // Not sure if this comparison will work or not
-      if (isEqual(prevCart, cart)) {
-        const client = initialiseTsGql();
-        const newCart = await createCheckout(client);
-        setCart(newCart);
-        prevCartRef.current = cart;
+    const setCheckoutItem = (cart: Cart) => {
+      if (isBrowser) {
+        localStorage.setItem(localStorageKey, cart.id as string);
       }
+      setCart(cart);
     };
-    // If cart as been purchased, create a new cart
-    if (!cart || cart?.completedAt) {
-      getNewCart();
+    const initialiseCheckout = async () => {
+      const existingCheckoutId = isBrowser
+        ? localStorage.getItem(localStorageKey)
+        : null;
+
+      if (existingCheckoutId && existingCheckoutId !== 'null') {
+        try {
+          const existingCheckout = await client.checkout.fetch(
+            existingCheckoutId
+          );
+          if (!existingCheckout.completedAt) {
+            setCheckoutItem(existingCheckout);
+            return;
+          }
+        } catch (error) {
+          console.error('initialiseCheckout', error);
+          localStorage.removeItem(localStorageKey);
+        }
+      }
+
+      const newCheckout = await client.checkout.create();
+      setCheckoutItem(newCheckout);
+    };
+
+    initialiseCheckout();
+  }, [isBrowser, setCart]);
+
+  const addVariantToCart = async (
+    variantId: string,
+    quantity: number
+  ): Promise<void> => {
+    const checkoutId = cart.id;
+
+    const lineItemsToUpdate = [
+      {
+        variantId,
+        quantity,
+      },
+    ];
+
+    try {
+      const newCart = await client.checkout.addLineItems(
+        checkoutId,
+        lineItemsToUpdate
+      );
+      setCart(newCart);
+    } catch (error) {
+      console.error('addVariantToCart', error);
+      localStorage.removeItem(localStorageKey);
     }
-  }, [cart, setCart]);
+  };
+
+  const removeLineItem = async (
+    checkoutId: string,
+    lineItemId: string
+  ): Promise<void> => {
+    try {
+      const newCart = await client.checkout.removeLineItems(checkoutId, [
+        lineItemId,
+      ]);
+      setCart(newCart);
+    } catch (error) {
+      console.error('removeLineItem', error);
+      localStorage.removeItem(localStorageKey);
+    }
+  };
+
+  const updateLineItem = async (
+    checkoutId: string,
+    lineItemId: string,
+    quantity: number
+  ): Promise<void> => {
+    const lineItemsToUpdate = [
+      {
+        id: lineItemId,
+        quantity,
+      },
+    ];
+
+    try {
+      const newCart = await client.checkout.updateLineItems(
+        checkoutId,
+        lineItemsToUpdate
+      );
+      setCart(newCart);
+    } catch (error) {
+      console.error('updateLineItem', error);
+      localStorage.removeItem(localStorageKey);
+    }
+  };
+
   return (
     <StoreContext.Provider
       value={{
+        addVariantToCart,
         cart,
+        client,
+        removeLineItem,
         setCart,
+        updateLineItem,
       }}
     >
       {children}

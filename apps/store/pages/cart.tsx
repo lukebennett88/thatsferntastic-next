8d42846a86614ea1b1da30b19e065704b69f0469
Luke Bennett <luke.bennett@brighte.com.au>
2021-10-03 17:51:24 +1100

    Share type

--- apps/store/pages/cart.tsx
+++ apps/store/pages/cart.tsx
@@ -4,84 +4,12 @@ import React from 'react';
 import { Button } from '../components/button';
 import { Spinner } from '../components/spinner';
 import { useStoreContext } from '../context/store-context';
+import { LineItem } from '../types';
 import { formatPrice } from '../utils';
 import { useCartCount } from '../utils/hooks/use-cart-count/use-cart-count';
 
-// The ShopifyBuy types are wrong, this is just a placeholder I made from a JSON file
-interface Product {
-  id: string;
-  title: string;
-  variant: {
-    id: string;
-    title: string;
-    price: string;
-    priceV2: {
-      amount: string;
-      currencyCode: string;
-    };
-    presentmentPrices: Array<{
-      price: {
-        amount: string;
-        currencyCode: string;
-      };
-      compareAtPrice: null;
-      hasNextPage: {
-        value: boolean;
-      };
-      hasPreviousPage: boolean;
-      variableValues: {
-        checkoutId: string;
-        lineItems: Array<{
-          variantId: string;
-          quantity: number;
-        }>;
-      };
-    }>;
-    weight: number;
-    available: boolean;
-    sku: string;
-    compareAtPrice: null;
-    compareAtPriceV2: null;
-    image: {
-      id: string;
-      src: string;
-      altText: null;
-    };
-    selectedOptions: Array<{
-      name: string;
-      value: string;
-    }>;
-    unitPrice: null;
-    unitPriceMeasurement: {
-      measuredType: null;
-      quantityUnit: null;
-      quantityValue: number;
-      referenceUnit: null;
-      referenceValue: number;
-    };
-    product: {
-      id: string;
-      handle: string;
-    };
-  };
-  quantity: number;
-  customAttributes: [];
-  discountAllocations: [];
-  hasNextPage: {
-    value: boolean;
-  };
-  hasPreviousPage: boolean;
-  variableValues: {
-    checkoutId: string;
-    lineItems: Array<{
-      variantId: string;
-      quantity: number;
-    }>;
-  };
-}
-
 interface CartPreviewItemProps {
-  product: Product;
+  product: LineItem;
   productIdx: number;
 }
 
--- apps/store/types/index.ts
+++ apps/store/types/index.ts
@@ -13,3 +13,76 @@ export type NextPageWithLayoutProps = NextPage & LayoutProps;
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type AnyObject = Record<string, any>;
+
+// The ShopifyBuy types are wrong, this is just a placeholder I made from a JSON file
+export interface LineItem {
+  id: string;
+  title: string;
+  variant: {
+    id: string;
+    title: string;
+    price: string;
+    priceV2: {
+      amount: string;
+      currencyCode: string;
+    };
+    presentmentPrices: Array<{
+      price: {
+        amount: string;
+        currencyCode: string;
+      };
+      compareAtPrice: null;
+      hasNextPage: {
+        value: boolean;
+      };
+      hasPreviousPage: boolean;
+      variableValues: {
+        checkoutId: string;
+        lineItems: Array<{
+          variantId: string;
+          quantity: number;
+        }>;
+      };
+    }>;
+    weight: number;
+    available: boolean;
+    sku: string;
+    compareAtPrice: null;
+    compareAtPriceV2: null;
+    image: {
+      id: string;
+      src: string;
+      altText: null;
+    };
+    selectedOptions: Array<{
+      name: string;
+      value: string;
+    }>;
+    unitPrice: null;
+    unitPriceMeasurement: {
+      measuredType: null;
+      quantityUnit: null;
+      quantityValue: number;
+      referenceUnit: null;
+      referenceValue: number;
+    };
+    product: {
+      id: string;
+      handle: string;
+    };
+  };
+  quantity: number;
+  customAttributes: [];
+  discountAllocations: [];
+  hasNextPage: {
+    value: boolean;
+  };
+  hasPreviousPage: boolean;
+  variableValues: {
+    checkoutId: string;
+    lineItems: Array<{
+      variantId: string;
+      quantity: number;
+    }>;
+  };
+}

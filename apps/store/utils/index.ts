406aa0dcd4a1d47ee5d46c24aecaf406eadf0eec
Luke Bennett <luke.bennett@brighte.com.au>
2021-10-03 14:32:05 +1100

    Add new utilty function

--- apps/store/utils/index.ts
+++ apps/store/utils/index.ts
@@ -1,3 +1,5 @@
+import type { Product } from '../graphql/get-product-by-handle';
+
 export function classNames(...classes: unknown[]): string {
   return classes.filter(Boolean).join(' ');
 }
@@ -54,3 +56,16 @@ export function slugify(text: string): string {
       ?.replace(/ +/g, '-') ?? ''
   );
 }
+
+// Taken from here: https://github.com/Shopify/js-buy-sdk/blob/28c85805ee21bdebf9611da6b92c39858b15dc0a/src/product-helpers.js#L21
+export const variantForOptions = (
+  product: NonNullable<Product>,
+  options: NonNullable<Product>['variants']['edges'][number]
+): NonNullable<Product>['variants']['edges'][number] | undefined => {
+  return product.variants.edges.find(variant => {
+    return variant.node.selectedOptions.every(selectedOption => {
+      // @ts-expect-error I think types might be too strict here
+      return options[selectedOption.name] === selectedOption.value.valueOf();
+    });
+  });
+};

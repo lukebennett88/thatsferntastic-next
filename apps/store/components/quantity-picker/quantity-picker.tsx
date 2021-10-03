593365f78e8fac92a0ef2909835478987fd56fd9
Luke Bennett <luke.bennett@brighte.com.au>
2021-10-03 17:31:35 +1100

    Add margin wrapper to QuantityPicker

--- apps/store/components/quantity-picker/quantity-picker.tsx
+++ apps/store/components/quantity-picker/quantity-picker.tsx
@@ -30,26 +30,29 @@ export function QuantityPicker({
   };
 
   return (
-    <span className="relative z-0 inline-flex rounded-md shadow-sm">
-      <button
-        type="button"
-        onClick={decrement}
-        className="relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-l-md hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-pink-500 focus:border-pink-500"
-      >
-        <span className="sr-only">Decrement</span>
-        <ChevronLeftIcon className="w-5 h-5" aria-hidden="true" />
-      </button>
-      <span className="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-gray-700 bg-white border border-gray-300 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-pink-500 focus:border-pink-500">
-        {quantity}
+    <div className="mt-6">
+      <div className="block text-sm font-medium text-gray-700">Quantity</div>
+      <span className="relative z-0 inline-flex mt-1 rounded-md shadow-sm">
+        <button
+          type="button"
+          onClick={decrement}
+          className="relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-l-md hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-pink-500 focus:border-pink-500"
+        >
+          <span className="sr-only">Decrement</span>
+          <ChevronLeftIcon className="w-5 h-5" aria-hidden="true" />
+        </button>
+        <span className="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-gray-700 bg-white border border-gray-300 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-pink-500 focus:border-pink-500">
+          {quantity}
+        </span>
+        <button
+          type="button"
+          onClick={increment}
+          className="relative inline-flex items-center px-2 py-2 -ml-px text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-r-md hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-pink-500 focus:border-pink-500"
+        >
+          <span className="sr-only">Increment</span>
+          <ChevronRightIcon className="w-5 h-5" aria-hidden="true" />
+        </button>
       </span>
-      <button
-        type="button"
-        onClick={increment}
-        className="relative inline-flex items-center px-2 py-2 -ml-px text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-r-md hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-pink-500 focus:border-pink-500"
-      >
-        <span className="sr-only">Increment</span>
-        <ChevronRightIcon className="w-5 h-5" aria-hidden="true" />
-      </button>
-    </span>
+    </div>
   );
 }

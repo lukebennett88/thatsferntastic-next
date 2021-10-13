2a07e8fe97e4dc0688a3e4199339a3c23e51a519
Luke Bennett <luke.bennett@brighte.com.au>
2021-10-13 20:46:50 +1100

    Move unused components out of productHandle

--- apps/store/pages/products/[productHandle].tsx
+++ apps/store/pages/products/[productHandle].tsx
@@ -1,6 +1,4 @@
-/* eslint-disable @next/next/no-img-element */
-import { Disclosure, Tab } from '@headlessui/react';
-import { HeartIcon, MinusSmIcon, PlusSmIcon } from '@heroicons/react/outline';
+import { Tab } from '@headlessui/react';
 import { Button } from '@thatsferntastic/button';
 import { classNames, formatPrice } from '@thatsferntastic/utils';
 import isEqual from 'lodash/isEqual';
@@ -22,66 +20,6 @@ import {
 import { variantForOptions } from '../../utils';
 import { addApolloState, initialiseTsGql } from '../../utils/apollo-client';
 import { siteSettings } from '../../utils/constants';
-
-const product = {
-  name: 'Zip Tote Basket',
-  price: '$140',
-  rating: 4,
-  images: [
-    {
-      id: 1,
-      name: 'Angled view',
-      src: 'https://tailwindui.com/img/ecommerce-images/product-page-03-product-01.jpg',
-      alt: 'Angled front view with bag zipped and handles upright.',
-    },
-    // More images...
-  ],
-  colors: [
-    {
-      name: 'Washed Black',
-      bgColor: 'bg-gray-700',
-      selectedColor: 'ring-gray-700',
-    },
-    { name: 'White', bgColor: 'bg-white', selectedColor: 'ring-gray-400' },
-    {
-      name: 'Washed Gray',
-      bgColor: 'bg-gray-500',
-      selectedColor: 'ring-gray-500',
-    },
-  ],
-  description: `
-    <p>The Zip Tote Basket is the perfect midpoint between shopping tote and comfy backpack. With convertible straps, you can hand carry, should sling, or backpack this convenient and spacious bag. The zip top and durable canvas construction keeps your goods protected for all-day use.</p>
-  `,
-  details: [
-    {
-      name: 'Features',
-      items: [
-        'Multiple strap configurations',
-        'Spacious interior with top zip',
-        'Leather handle and tabs',
-        'Interior dividers',
-        'Stainless strap loops',
-        'Double stitched construction',
-        'Water-resistant',
-      ],
-    },
-    // More sections...
-  ],
-};
-const relatedProducts = [
-  {
-    id: 1,
-    name: 'Zip Tote Basket',
-    color: 'White and black',
-    href: '#',
-    imageSrc:
-      'https://tailwindui.com/img/ecommerce-images/product-page-03-related-product-01.jpg',
-    imageAlt:
-      'Front of zip tote bag with white canvas, black canvas straps and handle, and black zipper pulls.',
-    price: '$140',
-  },
-  // More products...
-];
 
 interface ProductPageProps {
   product: NonNullable<Product>;
@@ -105,7 +43,7 @@ export const getServerSideProps: GetServerSideProps<ProductPageProps> = async ({
 };
 
 export default function ProductPage({
-  product: shopifyProduct,
+  product,
 }: InferGetServerSidePropsType<typeof getServerSideProps>): JSX.Element {
   const {
     availableForSale,
@@ -116,13 +54,14 @@ export default function ProductPage({
     priceRange,
     title,
     variants,
-  } = shopifyProduct;
+  } = product;
+
   // State for selected variant
   const [initialVariant] = variants.edges;
   const [variant, setVariant] = React.useState({
     ...initialVariant,
   });
-  const productVariant = variantForOptions(shopifyProduct, variant) || variant;
+  const productVariant = variantForOptions(product, variant) || variant;
 
   const handleOptionChange = (value: string, index: number) => {
     const currentOptions = [...variant.node.selectedOptions];
@@ -154,7 +93,7 @@ export default function ProductPage({
 
   const router = useRouter();
 
-  const { addVariantToCart, checkout } = useStoreContext();
+  const { addVariantToCart } = useStoreContext();
 
   const addToCart = async () => {
     await addVariantToCart(productVariant.node.id, quantity);
@@ -200,7 +139,7 @@ export default function ProductPage({
         ]}
       />
 
-      <main className="mx-auto max-w-7xl sm:pt-16 sm:px-6 lg:px-8">
+      <main className="pb-16 mx-auto max-w-7xl sm:py-16 sm:px-6 lg:px-8">
         <div className="max-w-2xl mx-auto lg:max-w-none">
           {/* Product */}
           <div className="lg:grid lg:grid-cols-2 lg:gap-x-8 lg:items-start">
@@ -327,127 +266,13 @@ export default function ProductPage({
                     </span>
                   </Button>
 
-                  <button
-                    type="button"
-                    className="flex items-center justify-center px-3 py-3 ml-4 text-gray-400 rounded-md hover:bg-gray-100 hover:text-gray-500"
-                  >
-                    <HeartIcon
-                      aria-hidden="true"
-                      className="flex-shrink-0 w-6 h-6"
-                    />
-                    <span className="sr-only">Add to favorites</span>
-                  </button>
+                  {/* Add to favourites */}
                 </div>
               </div>
-
-              <section aria-labelledby="details-heading" className="mt-12">
-                <h2 id="details-heading" className="sr-only">
-                  Additional details
-                </h2>
-
-                <div className="border-t divide-y divide-gray-200">
-                  {product.details.map(detail => (
-                    <Disclosure as="div" key={detail.name}>
-                      {({ open }) => (
-                        <>
-                          <h3>
-                            <Disclosure.Button className="relative flex items-center justify-between w-full py-6 text-left group">
-                              <span
-                                className={classNames(
-                                  open ? 'text-pink-600' : 'text-gray-900',
-                                  'text-sm font-medium'
-                                )}
-                              >
-                                {detail.name}
-                              </span>
-                              <span className="flex items-center ml-6">
-                                {open ? (
-                                  <MinusSmIcon
-                                    aria-hidden="true"
-                                    className="block w-6 h-6 text-pink-400 group-hover:text-pink-500"
-                                  />
-                                ) : (
-                                  <PlusSmIcon
-                                    aria-hidden="true"
-                                    className="block w-6 h-6 text-gray-400 group-hover:text-gray-500"
-                                  />
-                                )}
-                              </span>
-                            </Disclosure.Button>
-                          </h3>
-                          <Disclosure.Panel
-                            as="div"
-                            className="pb-6 prose-sm prose"
-                          >
-                            <pre>{JSON.stringify(checkout, null, 2)}</pre>
-                            {/* <ul role="list">
-                              {detail.items.map(item => (
-                                <li key={item}>{item}</li>
-                              ))}
-                            </ul> */}
-                          </Disclosure.Panel>
-                        </>
-                      )}
-                    </Disclosure>
-                  ))}
-                </div>
-              </section>
+              {/* Details */}
             </div>
           </div>
-
-          <section
-            aria-labelledby="related-heading"
-            className="px-4 py-16 mt-10 border-t border-gray-200 sm:px-0"
-          >
-            <h2
-              id="related-heading"
-              className="text-xl font-bold text-gray-900"
-            >
-              Customers also bought
-            </h2>
-
-            <div className="grid grid-cols-1 mt-8 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
-              {relatedProducts.map(product => (
-                <div key={product.id}>
-                  <div className="relative">
-                    <div className="relative w-full overflow-hidden rounded-lg h-72">
-                      <img
-                        src={product.imageSrc}
-                        alt={product.imageAlt}
-                        className="object-cover object-center w-full h-full"
-                      />
-                    </div>
-                    <div className="relative mt-4">
-                      <h3 className="text-sm font-medium text-gray-900">
-                        {product.name}
-                      </h3>
-                      <p className="mt-1 text-sm text-gray-500">
-                        {product.color}
-                      </p>
-                    </div>
-                    <div className="absolute inset-x-0 top-0 flex items-end justify-end p-4 overflow-hidden rounded-lg h-72">
-                      <div
-                        aria-hidden="true"
-                        className="absolute inset-x-0 bottom-0 opacity-50 h-36 bg-gradient-to-t from-black"
-                      />
-                      <p className="relative text-lg font-semibold text-white">
-                        {product.price}
-                      </p>
-                    </div>
-                  </div>
-                  <div className="mt-6">
-                    <a
-                      href={product.href}
-                      className="relative flex items-center justify-center px-8 py-2 text-sm font-medium text-gray-900 bg-gray-100 border border-transparent rounded-md hover:bg-gray-200"
-                    >
-                      Add to cart
-                      <span className="sr-only">, {product.name}</span>
-                    </a>
-                  </div>
-                </div>
-              ))}
-            </div>
-          </section>
+          {/* Related products */}
         </div>
       </main>
       <CartSidebar />

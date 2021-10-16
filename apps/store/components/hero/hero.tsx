5f054e34df91c98818c080d9c17549e703c06e9c
Luke Bennett <luke.bennett@brighte.com.au>
2021-10-17 10:19:02 +1100

    Update hero

--- apps/store/components/hero/hero.tsx
+++ apps/store/components/hero/hero.tsx
@@ -8,23 +8,22 @@ export function Hero(): JSX.Element {
       <div aria-hidden="true" className="absolute inset-0 overflow-hidden">
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
-          src="https://tailwindui.com/img/ecommerce-images/home-page-01-hero-full-width.jpg"
+          src="/bees.jpg"
           alt=""
           className="object-cover object-center w-full h-full"
         />
       </div>
       <div
         aria-hidden="true"
-        className="absolute inset-0 bg-gray-900 opacity-50"
+        className="absolute inset-0 bg-gray-900 opacity-60"
       />
       <div className="relative flex flex-col items-center max-w-3xl px-6 py-32 mx-auto text-center sm:py-64 lg:px-0">
         <h1 className="text-4xl font-extrabold tracking-tight text-white lg:text-6xl">
-          New arrivals are here
+          Bees!
         </h1>
         <p className="mt-4 text-xl text-white">
-          The new arrivals have, well, newly arrived. Check out the latest
-          options from our summer small-batch release while they&lsquo;re still
-          in stock.
+          New pencil cases, mini pouches, keyfobs, scrunchies and planner
+          charms! Everything limited stock so get in early 🐝🌻
         </p>
         <div className="mt-8">
           <Link href="/collections" passHref>

e2c6d6b4f8f9c8bb7b39e0b2618b458af94a2891
Luke Bennett <hello@lukebennett.com.au>
2022-01-23 18:34:10 +1100

    Attempt to fix vercel deploy 2

--- apps/remix-ecommerce/rollup.config.js
+++ apps/remix-ecommerce/rollup.config.js
@@ -1,18 +1,25 @@
+// @ts-check
 import commonJs from "@rollup/plugin-commonjs";
 import jsonPlugin from "@rollup/plugin-json";
 import resolve from "@rollup/plugin-node-resolve";
 
-export default {
+/** @type {import('rollup').RollupOptions} */
+const rollupConfig = {
   input: "api/api.js",
   output: {
     file: "api/index.js",
     format: "cjs",
+    exports: "auto",
   },
+  external: ["fs/promises", "fs", "https", "path", "crypto", "stream", "http", "zlib", "os", "child_process"],
   plugins: [
     commonJs(),
     resolve({
       moduleDirectories: ["node_modules"],
+      preferBuiltins: false,
     }),
     jsonPlugin(),
   ],
 };
+
+export default rollupConfig;

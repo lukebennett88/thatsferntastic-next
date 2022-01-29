8c5a1ffa9bf8e0f559502dd2afea89c9263963e0
Luke Bennett <hello@lukebennett.com.au>
2022-01-29 13:57:00 +1100

    Updates

--- apps/remix-ecommerce/app/components/footer.tsx
+++ apps/remix-ecommerce/app/components/footer.tsx
@@ -1,9 +1,6 @@
 import type { To } from "react-router-dom";
 import { Link } from "remix";
 
-import { GithubIcon } from "./icons";
-import { OptimizedImage } from "./optimized-image";
-
 export type FooterPage = {
   id: string;
   title: string;
@@ -12,39 +9,251 @@ export type FooterPage = {
 
 export function Footer({ logoHref, pages, storeName }: { logoHref: string; pages: FooterPage[]; storeName?: string }) {
   return (
-    <footer>
-      <div className="border-t border-zinc-700 px-4 py-16 lg:flex lg:px-6">
-        <div className="mb-6 lg:mr-20">
-          <div className="flex flex-1 items-center">
-            <img className="h-10 w-10" src={logoHref} alt="" width={40} height={40} />
-            {storeName ? <h1 className="ml-4 text-lg font-semibold">{storeName}</h1> : null}
+    <footer aria-labelledby="footer-heading" className="bg-gray-50">
+      <h2 id="footer-heading" className="sr-only">
+        Footer
+      </h2>
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="border-t border-gray-200 py-20">
+          <div className="grid grid-cols-1 md:grid-flow-col md:auto-rows-min md:grid-cols-12 md:gap-x-8 md:gap-y-16">
+            <div className="col-span-1 md:col-span-2 lg:col-start-1 lg:row-start-1">
+              <a className="inline-flex rounded-full focus:ring" href="/">
+                <span className="sr-only">{storeName}</span>
+                <img src={logoHref} alt="" width={32} height={32} />
+              </a>
+            </div>
+            <div className="col-span-6 mt-10 grid grid-cols-2 gap-8 sm:grid-cols-3 md:col-span-8 md:col-start-3 md:row-start-1 md:mt-0 lg:col-span-6 lg:col-start-2">
+              <div className="grid grid-cols-1 gap-y-12 sm:col-span-2 sm:grid-cols-2 sm:gap-x-8">
+                <div>
+                  <h3 className="text-sm font-medium text-gray-900">Collections</h3>
+                  <ul role="list" className="mt-6 space-y-6">
+                    <li className="text-sm">
+                      <a className="text-gray-500 hover:text-gray-600" href="/collections/advent-scrunchie-boxes">
+                        Advent Scrunchie Boxes
+                      </a>
+                    </li>
+                    <li className="text-sm">
+                      <a className="text-gray-500 hover:text-gray-600" href="/collections/animals">
+                        Animals
+                      </a>
+                    </li>
+                    <li className="text-sm">
+                      <a className="text-gray-500 hover:text-gray-600" href="/collections/australian-artists">
+                        Australian Artists
+                      </a>
+                    </li>
+                    <li className="text-sm">
+                      <a className="text-gray-500 hover:text-gray-600" href="/collections/dogs">
+                        Dog Breeds
+                      </a>
+                    </li>
+                    <li className="text-sm">
+                      <a className="text-gray-500 hover:text-gray-600" href="/collections/florals-patterns">
+                        Florals &amp; Patterns
+                      </a>
+                    </li>
+                    <li className="text-sm">
+                      <a className="text-gray-500 hover:text-gray-600" href="/collections/gaming">
+                        Gaming
+                      </a>
+                    </li>
+                    <li className="text-sm">
+                      <a className="text-gray-500 hover:text-gray-600" href="/collections/gift-sets">
+                        Gift Sets
+                      </a>
+                    </li>
+                    <li className="text-sm">
+                      <a className="text-gray-500 hover:text-gray-600" href="/collections/halloween">
+                        Halloween
+                      </a>
+                    </li>
+                    <li className="text-sm">
+                      <a className="text-gray-500 hover:text-gray-600" href="/collections/katherine-appleby">
+                        Katherine Appleby
+                      </a>
+                    </li>
+                    <li className="text-sm">
+                      <a className="text-gray-500 hover:text-gray-600" href="/collections/scrunchies">
+                        Scrunchies
+                      </a>
+                    </li>
+                    <li className="text-sm">
+                      <a
+                        className="text-gray-500 hover:text-gray-600"
+                        href="/collections/pens-and-pencils-for-the-stationery-nerds"
+                      >
+                        Stationery Nerds
+                      </a>
+                    </li>
+                    <li className="text-sm">
+                      <a className="text-gray-500 hover:text-gray-600" href="/collections/characters">
+                        TV + Movies
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+                <div>
+                  <h3 className="text-sm font-medium text-gray-900">Product categories</h3>
+                  <ul role="list" className="mt-6 space-y-6">
+                    <li className="text-sm">
+                      <a href="/categories?type=Person Accessories" className="text-gray-500 hover:text-gray-600">
+                        Person Accessories
+                      </a>
+                    </li>
+                    <li className="text-sm">
+                      <a href="/categories?type=Pocket Pouches" className="text-gray-500 hover:text-gray-600">
+                        Pocket Pouches
+                      </a>
+                    </li>
+                    <li className="text-sm">
+                      <a href="/categories?type=Washi Tape" className="text-gray-500 hover:text-gray-600">
+                        Washi Tape
+                      </a>
+                    </li>
+                    <li className="text-sm">
+                      <a href="/categories?type=Stationery" className="text-gray-500 hover:text-gray-600">
+                        Stationery
+                      </a>
+                    </li>
+                    <li className="text-sm">
+                      <a href="/categories?type=Mini Pouches" className="text-gray-500 hover:text-gray-600">
+                        Mini Pouches
+                      </a>
+                    </li>
+                    <li className="text-sm">
+                      <a href="/categories?type=Planner Accessories" className="text-gray-500 hover:text-gray-600">
+                        Planner Accessories
+                      </a>
+                    </li>
+                    <li className="text-sm">
+                      <a href="/categories?type=Pencil Cases" className="text-gray-500 hover:text-gray-600">
+                        Pencil Cases
+                      </a>
+                    </li>
+                    <li className="text-sm">
+                      <a href="/categories?type=Gift Sets" className="text-gray-500 hover:text-gray-600">
+                        Gift Sets
+                      </a>
+                    </li>
+                    <li className="text-sm">
+                      <a href="/categories?type=Stickers" className="text-gray-500 hover:text-gray-600">
+                        Stickers
+                      </a>
+                    </li>
+                    <li className="text-sm">
+                      <a href="/categories?type=Scrunchies" className="text-gray-500 hover:text-gray-600">
+                        Scrunchies
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div>
+                <h3 className="text-sm font-medium text-gray-900">Customer Service</h3>
+                <ul role="list" className="mt-6 space-y-6">
+                  <li className="text-sm">
+                    <a className="text-gray-500 hover:text-gray-600" href="/about">
+                      About
+                    </a>
+                  </li>
+                  <li className="text-sm">
+                    <a className="text-gray-500 hover:text-gray-600" href="/privacy-policy">
+                      Privacy Policy
+                    </a>
+                  </li>
+                  <li className="text-sm">
+                    <a className="text-gray-500 hover:text-gray-600" href="/refund-policy">
+                      Refund Policy
+                    </a>
+                  </li>
+                  <li className="text-sm">
+                    <a className="text-gray-500 hover:text-gray-600" href="/shipping-and-processing-times">
+                      Shipping and Processing Times
+                    </a>
+                  </li>
+                  <li className="text-sm">
+                    <a className="text-gray-500 hover:text-gray-600" href="/terms-of-service">
+                      Terms of Service
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="mt-12 md:col-span-8 md:col-start-3 md:row-start-2 md:mt-0 lg:col-span-4 lg:col-start-9 lg:row-start-1">
+              <div>
+                <h3 className="text-sm font-medium text-gray-900">Sign up for our newsletter</h3>
+                <p className="mt-6 text-sm text-gray-500">
+                  Intermittent updates, when I remember to send them, about new releases, sales and good news!
+                </p>
+                <form className="mt-2 flex sm:max-w-md">
+                  <label htmlFor="email-address" className="sr-only">
+                    Email address
+                  </label>
+                  <input
+                    type="text"
+                    id="email-address"
+                    autoComplete="email"
+                    required
+                    placeholder="Enter your email"
+                    className="w-full min-w-0 appearance-none rounded-full border border-gray-300 bg-white px-4 py-2 text-base text-gray-900 placeholder-gray-500 focus:border-pink-500 focus:outline-none focus:ring-1 focus:ring-pink-500 sm:text-sm"
+                  />
+                  <div className="ml-4 flex-shrink-0">
+                    <button
+                      className="inline-flex transform-gpu items-center justify-center rounded-full border border-transparent bg-pink-200 px-5 py-2 text-center font-mono text-base font-medium text-pink-700 transition duration-300 ease-in-out hover:bg-pink-100 hover:text-pink-600 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2"
+                      type="submit"
+                    >
+                      Sign up
+                    </button>
+                  </div>
+                </form>
+              </div>
+              <div className="mt-12">
+                <h3 className="text-sm font-medium text-gray-900">{storeName}</h3>
+                <p className="mt-6 text-sm text-gray-500">
+                  {storeName} is an online store focusing on the little things that make you smile. Pencil cases,
+                  planner pouches and coin purses, all handmade in Australia, plus accessories, stationery and stickers
+                  to add a little more sunshine to your day. Offering flat rate shipping from my little studio on the
+                  coast of NSW.
+                </p>
+                <div className="mt-6 flex space-x-6">
+                  <a href="https://www.facebook.com/thatsferntastic/" className="text-gray-400 hover:text-gray-500">
+                    <span className="sr-only">Facebook</span>
+                    <svg fill="currentColor" aria-hidden="true" viewBox="0 0 24 24" className="h-6 w-6">
+                      <path
+                        fillRule="evenodd"
+                        d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </a>
+                  <a href="https://www.instagram.com/thatsferntastic/" className="text-gray-400 hover:text-gray-500">
+                    <span className="sr-only">Instagram</span>
+                    <svg fill="currentColor" aria-hidden="true" viewBox="0 0 24 24" className="h-6 w-6">
+                      <path
+                        fillRule="evenodd"
+                        d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </a>
+                  <a href="https://www.twitter.com/thatsferntastic/" className="text-gray-400 hover:text-gray-500">
+                    <span className="sr-only">Twitter</span>
+                    <svg fill="currentColor" aria-hidden="true" viewBox="0 0 24 24" className="h-6 w-6">
+                      <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        <ul className="mb-6 mr-auto grid max-w-lg flex-1 gap-4 md:grid-flow-col md:grid-rows-4">
-          {pages.map((page) => (
-            <li key={page.id}>
-              <Link className="hover:text-gray-300 focus:text-gray-300" prefetch="intent" to={page.to}>
-                {page.title}
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <div>
-          <div className="flex items-center">
-            <a
-              className="mr-4 inline-block hover:text-gray-300 focus:text-gray-300"
-              href="https://github.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <span className="sr-only">View source</span>
-              <GithubIcon className="h-8 w-8" />
-            </a>
-          </div>
+        <div className="border-t border-gray-200 py-10 text-center">
+          <p className="text-sm text-gray-500">
+            © {/* */}2022{/* */} {/* */}
+            {storeName}
+            {/* */}. All rights reserved.
+          </p>
         </div>
-      </div>
-      <div className="border-t border-zinc-700 px-4 py-8 lg:px-6">
-        <p className="text-sm text-gray-300">© 2022 {storeName}. All rights reserved.</p>
       </div>
     </footer>
   );
